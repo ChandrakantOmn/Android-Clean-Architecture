bc860036d9e5f44b54adbfe0a12446d0dc81f941
MSI\Anthony <a173200488@gmail.com>
2023-05-27 02:26:35 +0800

    refactor: LoginRepositoryImpl

--- app/src/main/java/com/anthony/net/sample/github/data/repository/login/LoginRepositoryImpl.kt
+++ app/src/main/java/com/anthony/net/sample/github/data/repository/login/LoginRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.anthony.net.sample.github.data.repository.login
 
+import com.anthony.net.sample.github.data.remote.model.common.UserDto
 import com.anthony.net.sample.github.data.remote.network.Resource
 import com.anthony.net.sample.github.data.remote.service.login.LoginService
 import com.anthony.net.sample.github.domain.model.common.User
@@ -14,10 +15,38 @@ class LoginRepositoryImpl(private val loginService: LoginService) : LoginReposit
     override suspend fun getUser(userName: String): Flow<Resource<User>> = flow {
         emit(Resource.Loading)
         val originData = loginService.getUser(userName)
-        val dataMapping = User(originData.login)
+        val dataMapping = mappingOriginDataToUser(originData)
         emit(Resource.Success(dataMapping))
     }.catch {
         emit(Resource.Error(it.localizedMessage ?: "An unknown error occurred"))
     }.flowOn(Dispatchers.IO)
+
+
+    private fun mappingOriginDataToUser(originData: UserDto): User {
+        return User(
+            id = originData.id,
+            login = originData.login,
+            avatar_url = originData.avatar_url,
+            email = originData.email,
+            events_url = originData.events_url,
+            followers = originData.followers,
+            followers_url = originData.followers_url,
+            following = originData.following,
+            following_url = originData.following_url,
+            gists_url = originData.gists_url,
+            gravatar_id = originData.gravatar_id,
+            html_url = originData.html_url,
+            organizations_url = originData.organizations_url,
+            public_gists = originData.public_gists,
+            public_repos = originData.public_repos,
+            received_events_url = originData.received_events_url,
+            repos_url = originData.repos_url,
+            site_admin = originData.site_admin,
+            starred_url = originData.starred_url,
+            subscriptions_url = originData.subscriptions_url,
+            twitter_username = originData.twitter_username,
+            url = originData.url
+        )
+    }
 
 }
--- app/src/main/java/com/anthony/net/sample/github/domain/model/common/User.kt
+++ app/src/main/java/com/anthony/net/sample/github/domain/model/common/User.kt
@@ -3,4 +3,27 @@ package com.anthony.net.sample.github.domain.model.common
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class User(val login: String)
+data class User(
+    val id: Int,
+    val login: String,
+    val avatar_url: String,
+    val email: String? = null,
+    val events_url: String,
+    val followers: Int,
+    val followers_url: String,
+    val following: Int,
+    val following_url: String,
+    val gists_url: String,
+    val gravatar_id: String,
+    val html_url: String,
+    val organizations_url: String,
+    val public_gists: Int,
+    val public_repos: Int,
+    val received_events_url: String,
+    val repos_url: String,
+    val site_admin: Boolean,
+    val starred_url: String,
+    val subscriptions_url: String,
+    val twitter_username: String? = null,
+    val url: String
+)
